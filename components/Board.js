0d7234d3af27d54742195997fa4735649110bbe9
Gabriel <Gabriel Acosta>
2017-10-29 12:18:31 -0600

    Almost done

--- components/Board.js
+++ components/Board.js
@@ -2,10 +2,18 @@ import React from 'react';
 import Field from './Field';
 
 export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
   render () {
     const { board, onClick } = this.props;
+    const fields = board.map((field, index) => {
+      return <Field player={field} key={index}  onClick={onClick.bind(null, index)} />
+    });
     return (
-      <div>
+      <div className='board'>
+        {fields}
       </div>
     );
   }
--- components/Game.js
+++ components/Game.js
@@ -6,27 +6,62 @@ import solutions from './solutions';
 export default class Game extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      board: [
+        null, null, null,
+        null, null, null,
+        null, null, null,
+      ],
+      turn: 'X'
+    };
 
     this.handleReset = this.handleReset.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.isComplete = this.isComplete.bind(this);
+    this.getWinner = this.getWinner.bind(this);
+
   }
 
   handleReset (ev) {
+    this.setState({
+      board: [
+        null, null, null,
+        null, null, null,
+        null, null, null,
+      ],
+      turn: 'X'
+    });
+    ev.preventDefault();
+
   }
 
   handleClick (i, ev) {
+    ev.preventDefault();
+    const updatedBoard = this.state.board.map((field, index) => {
+      return index === i ? this.state.turn : field
+    })
+    this.setState({
+      turn: this.state.turn === 'X' ? 'O' : 'X',
+      board: updatedBoard
+    })
+
   }
 
   getWinner () {
+
   }
 
   isComplete () {
+    num = this.state.board.filter( x => x) // Return only valid or not null values
+    return num.length === this.state.board.length
   }
 
   render () {
+
     return (
-      <div>
+      <div className='game'>
+        <Board board={this.state.board} onClick={this.handleClick}/>
+        <button className='game__reset' onClick={this.handleReset}>Reset</button>
       </div>
     );
   }
